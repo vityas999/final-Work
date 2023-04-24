11bc76d39ca6b5f9e07e91d70dd7e6b74571ddd6
Vitalik <vitalijzaxarov99@mail.ru>
2023-04-24 10:02:26 +0300

    Добавили код

---
+++ FinalTask/Program.cs
@@ -0,0 +1,48 @@
+﻿string[] ArrayWithIf(string[] array1, string[] array2)
+{
+    int count = 0;
+    int stop = 3;
+    for (int i = 0; i < array2.Length; i++)
+    {
+        if (i < array2.Length && array1[i].Length <= stop)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+    int lengthResultArray = count;
+    string[] resultArray = new string[lengthResultArray];
+    for (int i = 0; i < lengthResultArray; i++)
+    {
+        resultArray[i] = array2[i];
+    }
+    return resultArray;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        string thing = ", ";
+        if (i == 0 && array.Length == 1) Console.Write("[" + array[i] + "]");
+        else if (i == 0 && array.Length != 1) Console.Write("[" + array[i] + thing);
+        else
+        {
+            if (i != array.Length - 1) Console.Write(array[i] + thing);
+            else Console.Write(array[i] + "]");
+        }
+    }
+    Console.WriteLine();
+}
+
+void main()
+{
+    string[] array = new string[6] { "124", "that", "0000", "8553535", "it", "not" };
+    string[] sortArray = new string[array.Length];
+    string[] result = ArrayWithIf(array, sortArray);
+    PrintArray(array);
+    PrintArray(result);
+}
+
+main();
